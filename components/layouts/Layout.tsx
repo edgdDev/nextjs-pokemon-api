a51ca5b6a084473bded12c9d79043540bca2371b
Elvin Guzman <elvinguzmandiaz@iElvinDG.local>
2022-08-27 18:23:12 -0400

    First commit

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,38 @@
+import { FC, ReactNode } from "react"
+import Head from "next/head"
+import { Navbar } from '../ui';
+
+interface Props {
+    children: ReactNode;
+    title?: string
+}
+
+const origin = (typeof window === 'undefined')? '' : window.location.origin
+ 
+ export const Layout: FC<Props> = ({ children, title }) => {
+   return (
+     <>
+        
+        <Head>
+            <title>{ title || 'Pokemon App' }</title>
+            <meta name="author" content="Elvin D. Guzmán"/>
+            <meta name="description" content={`Información sobre el pokemon: ${ title }`}/>
+            <meta name="keywords" content={`${ title }, pokemon, pokedex`}/>
+
+            <meta property="og:title" content={ `Información sobre ${ title }` } />
+            <meta property="og:description" content={ `Esta es la página sobre ${ title }` } />
+            <meta property="og:image" content={ `${ origin }/imgs/pokemon.png` } />
+        </Head>
+
+        <Navbar />
+               
+        <main style={{
+            padding: '0 20px'
+        }}>
+            { children }
+        </main>
+     
+     </>
+   )
+ }
+ 
